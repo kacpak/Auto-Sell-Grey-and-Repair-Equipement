b11cd28a4e89ac6e233435f3990ff0faa48e8e7f
Strik3ria <robert_coones@zohomail.com>
2022-11-23 09:55:05 -0600

    Added my_locale to locals

--- kSellGreyNRepair.lua
+++ kSellGreyNRepair.lua
@@ -17,7 +17,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ]]
 
-local localization = ...
+local localization, my_locale = ...
 
 local function OnEvent(self, event)
 	-- Auto Sell Grey Items
